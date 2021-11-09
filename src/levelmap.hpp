118c7d7763dc3059d3d067a70572df1477f90aba
Nuutti Nykänen <nuutti.nykanen@aalto.fi>
2021-11-09 17:27:34 +0200

    LevelMap constructor

--- src/levelmap.hpp
+++ src/levelmap.hpp
@@ -5,32 +5,65 @@
 class LevelMap {
 
   public:
-  LevelMap(size_t size) : size_(size) {
+  LevelMap(size_t size, std::vector<std::pair<std::pair<int, int>, std::pair<int, int>>> enemy_path) : size_(size) {
     towers_ = std::vector<std::string>();
     enemies_ = std::vector<std::string>();
-    squares_ = std::map<std::pair<int, int>, MapSquare*>();
+    squares_ = std::map<std::pair<int, int>, FreeSquare*>();
+    e_path_begin_ = enemy_path.at(0).first;
+    e_path_end_ = enemy_path.at(enemy_path.size() - 1).second;
 
     for(size_t i = 0; i < size; i++) {
       for(size_t j = 0; j < size; j++) {
         std::pair<int, int> coords = std::make_pair(i, j);
-        MapSquare* new_square = new MapSquare();
-        std::pair<std::pair<int, int>, MapSquare*> ins = std::make_pair(coords, new MapSquare());
+        FreeSquare* new_square = new FreeSquare(i, j);
+        std::pair<std::pair<int, int>, FreeSquare*> ins = std::make_pair(coords, new FreeSquare(i, j));
         squares_.insert(ins);
       }
     }
-  }
+
+    for(auto it : enemy_path) {
+      std::pair<int, int> first = it.first;
+      std::pair<int, int> second = it.second;
+      while(it.first != it.second) {
+        std::pair<int, int> ins = std::make_pair(first.first, first.second);
+        EnemyPathSquare* new_square = new EnemyPathSquare(ins.first, ins.second);
+        ChangeSquare(ins.first, ins.second, new_square);
+        enemy_path_.insert(std::make_pair(std::make_pair(ins.first, ins.second), new_square));
+
+        // for ex. (2, 4) -> (2, 9)
+        if(first.first == second.first) {
+          if(first.second < second.second) first.second++;
+          else first.second--;
+        }
+        // for ex. (2, 4) -> (9, 4)
+        else {
+          if(first.first < second.first) first.first++;
+          else first.second++;
+        }
+      }
+    }
+   }
 
   
   
-  std::map<std::pair<int, int>, MapSquare*>& GetSquares() { return squares_; }
+  std::map<std::pair<int, int>, FreeSquare*>& GetSquares() { return squares_; }
   std::vector<std::string>& GetTowers() { return towers_; }
   std::vector<std::string>& GetEnemies()  { return enemies_; }
+
   MapSquare* GetSquare(int x, int y) const { return squares_.at(std::make_pair(x, y)); }
+
+  void ChangeSquare(int x, int y, MapSquare* new_square) {
+    MapSquare* prev_square = GetSquare(x, y);
+    prev_square = new_square;
+  }
 
   private:
   size_t size_;
-  std::map<std::pair<int, int>, MapSquare*> squares_;
-  std::map<std::pair<int, int>, MapSquare*> enemy_path_;
+  std::map<std::pair<int, int>, FreeSquare*> squares_;
+  std::map<std::pair<int, int>, EnemyPathSquare*> enemy_path_;
+  std::pair<int, int> e_path_begin_;
+  std::pair<int, int> e_path_end_;
   std::vector<std::string> towers_;
   std::vector<std::string> enemies_;
+  std::vector<std::string> projectiles_;
 };
--- src/mapsquare.hpp
+++ src/mapsquare.hpp
@@ -1,15 +1,52 @@
-
+#include "tower.hpp"
 class MapSquare {
     public:
-    MapSquare() {
-        is_enemy_path_ = true;
-        is_tower_ = true;
-        is_free_ = true;
-    };
-    
-    
+    MapSquare(int x, int y) : x_(x), y_(y) { }
+    ~MapSquare(){} 
+    virtual bool free() = 0;
+    virtual bool occupied() = 0;
     private: 
-    bool is_enemy_path_;
-    bool is_tower_;
-    bool is_free_;
+    int x_;
+    int y_;
 };
+
+class FreeSquare : public MapSquare { 
+
+    public: 
+    FreeSquare(int x, int y) : MapSquare(x, y){} 
+    ~FreeSquare(){} 
+    virtual bool free() { return true; } 
+    virtual bool occupied() { return false; }
+}; 
+
+class TowerSquare : public MapSquare { 
+
+    public: 
+    TowerSquare(int x, int y, Tower tower) : MapSquare(x, y), tower_(tower) {} 
+    ~TowerSquare(){} 
+    virtual bool free() { return false; } 
+    virtual bool occupied() { return true; } 
+    Tower& getTower() { return tower_; } 
+
+    private: 
+    Tower tower_; 
+}; 
+
+class EnemyPathSquare : public MapSquare { 
+    public: 
+    EnemyPathSquare(int x, int y) : MapSquare(x, y){} 
+    ~EnemyPathSquare(){} 
+    virtual bool free() { return false; } 
+    virtual bool occupied() { return false; } 
+}; 
+
+class EnemySquare : public EnemyPathSquare { 
+    public: 
+    EnemySquare(int x, int y, Enemy enemy) : EnemyPathSquare(x, y), enemy_(enemy){} 
+    ~EnemySquare(){} 
+    virtual bool free() { return false; } 
+    virtual bool occupied() { return true; }
+    Enemy& getEnemy() = { return enemy_ }
+    private:
+    Enemy enemy_;
+}; 
